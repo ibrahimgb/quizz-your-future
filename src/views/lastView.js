df984071e3fcf19b13cb9444c5cc7fda028bd05b
Suleyman Yilmaz <yilmazsgit@gmail.com>
2022-03-09 15:02:11 +0100

    Added class names to question elements.

--- src/views/lastView.js
+++ src/views/lastView.js
@@ -6,7 +6,7 @@ import { quizData } from "../data.js";
 export const createLastElement = () => {
   const element = document.createElement('div');
   element.innerHTML = String.raw`
-    <button id="${RETURN_HOME_BUTTON_ID}">return</button>
+    <button class="btn-primary" id="${RETURN_HOME_BUTTON_ID}">return</button>
     `;
   return element;
 };
@@ -25,11 +25,11 @@ export const questionAndAnswerList = () => {
         
         if(question.selected !== null) {
             liElement.innerHTML = String.raw`
-        <h3>${question.text}</h3>
+        <h3 class="qa-question">${question.text}</h3>
     
         <ul>
-            <li>Your selection: ${question.selected}: ${question.answers[question.selected]}</li>
-            <li>Correct answer: ${question.correct}: ${question.answers[question.correct]}</li>
+            <li class="qa-selection">Your selection: ${question.selected}: ${question.answers[question.selected]}</li>
+            <li class="qa-answer">Correct answer: ${question.correct}: ${question.answers[question.correct]}</li>
         </ul>
         `;
         element.appendChild(liElement);
